0a7601ecac31a06de95746bf5872484a955b3312
Ait Mansour Mohamed <uapv1802814@ecv-2w26-105.univ-avignon.fr>
2018-04-03 11:17:11 +0200

    Adding Param

--- Part 1/Q3- Hello Someone/Server/sender/Sender.java
+++ Part 1/Q3- Hello Someone/Server/sender/Sender.java
@@ -2,6 +2,7 @@ package sender;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/hello")
 public class Sender {
@@ -10,6 +11,13 @@ public class Sender {
     @GET()
     public String hello() {
         return "Hello";
+    }
+    
+    /* Premier exemple avec un GET */
+    @GET()
+	@Path("/{name}")
+    public String helloSomeOne(@PathParam("name") String name){
+        return "Hello : "+name;
     }
 
 }
