586e0a4b8ae9843c260521d35513a6fd9330f7a4
Ait Mansour Mohamed <uapv1802814@ecv-2w26-105.univ-avignon.fr>
2018-04-03 10:38:20 +0200

    Adding env var

--- Part 1/classpath.sh
+++ Part 1/classpath.sh
@@ -1,3 +1,4 @@
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/javax.ws.rs-api.jar
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-client.jar 
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-common.jar
+export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-guava.jar
