6d1703e1761a5214a2f5679fc1e49692da8adbac
Ait Mansour Mohamed <uapv1802814@ecv-2w26-105.univ-avignon.fr>
2018-04-03 11:08:51 +0200

    Xml HTML Example

--- Part 1/Q2- Hello Ola/Server/sender/Sender.java
+++ Part 1/Q2- Hello Ola/Server/sender/Sender.java
@@ -2,12 +2,27 @@ package sender;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/hello")
+@Produces("text/plain")
 public class Sender {
 
     @GET()
     public String hello() {
+        return "Hello";
+    }
+    
+    @GET()
+	@Path("/xml")
+    @Produces("application/xml")
+    public String salut() {
+        return "Hello";
+    }
+    @GET()
+	@Path("/html")
+    @Produces("text/html")
+    public String ola() {
         return "Hello";
     }
 
