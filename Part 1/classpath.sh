3c7bf1807ade610ef2fcf0c0d2dd487bc182094d
Ait Mansour Mohamed <uapv1802814@ecv-2w26-105.univ-avignon.fr>
2018-04-03 10:44:30 +0200

    First Question done

--- Part 1/classpath.sh
+++ Part 1/classpath.sh
@@ -2,3 +2,6 @@ export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/javax.w
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-client.jar 
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-common.jar
 export CLASSPATH=$CLASSPATH:/usr/local/stow/glassfish4/glassfish/modules/jersey-guava.jar
+
+
+javac -d WEB-INF/classes sender/*.java
